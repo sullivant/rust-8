3996412c6af983859f8b9a044411455702d380c1
Thomas Sullivan <thomas.l.sullivan@vumc.org>
2019-09-05 10:45:54 -0500

    migrated to a lib structure and put tests into their own file.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,5 @@
-mod cpu;
-mod fonts;
-
 use std::io;
-
-use cpu::Cpu;
-
 fn main() -> io::Result<()> {
-    let mut cpu = Cpu {
-        memory: [0; 4096],
-        opcode: 0,
-        v: [0; 16],
-        i: 0,
-        pc: 0,
-        gfx: [0; (64 * 32)],
-        delay_timer: 0,
-        sound_timer: 0,
-        stack: [0; 16],
-        sp: 0,
-    };
-    cpu.initialize();
-    cpu.load_rom("./data/PONG".to_string())?;
-
-    for _i in 1..5 {
-        cpu.tick();
-        cpu.dump_regs();
-    }
-
+    lib::go()?;
     Ok(())
 }
